8d897b5544f1b5eca62cc7dbe60a9a2f2529e1eb
Katlego Thubisi <katlego.thubisi@goodcode.co.za>
2024-04-29 18:14:05 +0200

    Update office

---
+++ tilesets/Abi logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Abi logo" tilewidth="32" tileheight="32" tilecount="169" columns="13">
+ <image source="../Abi Logo.png" width="432" height="432"/>
+</tileset>
---
+++ tilesets/Abi-Red-Logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Abi-Red-Logo" tilewidth="32" tileheight="32" tilecount="4489" columns="67">
+ <image source="../white-red-with-slogan.png" width="2160" height="2160"/>
+</tileset>
